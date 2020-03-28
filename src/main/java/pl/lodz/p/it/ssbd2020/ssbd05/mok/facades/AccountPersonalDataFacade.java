c4383a1cde3f22aa262d66f41e5d051d0f9f109c
Jakub Bogdan <kubab1998@o2.pl>
2020-03-28 19:32:39 +0100

    Wygenerowane klasy fasad

---
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/facades/AccountPersonalDataFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pl.lodz.p.it.ssbd2020.ssbd05.mok.facades;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import pl.lodz.p.it.ssbd2020.ssbd05.mok.entities.AccountPersonalData;
+
+
+@Stateless
+public class AccountPersonalDataFacade extends AbstractFacade<AccountPersonalData> {
+
+    @PersistenceContext(unitName = "ssbd05mokPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AccountPersonalDataFacade() {
+        super(AccountPersonalData.class);
+    }
+
+}
