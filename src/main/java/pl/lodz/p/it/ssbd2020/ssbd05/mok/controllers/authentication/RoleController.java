43de8fc83fb36fa36eec65f271e1a87c463fde38
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-03-28 19:40:09 +0100

    Zmiana roli przenosi na index, usunięcie zduplikowanego kodu w RoleController

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/controllers/authentication/RoleController.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/controllers/authentication/RoleController.java
@@ -3,12 +3,10 @@ package pl.lodz.p.it.ssbd2020.ssbd05.mok.controllers.authentication;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +17,16 @@ public class RoleController implements Serializable {
 
     @Getter @Setter
     private String selectedRole;
+    private ExternalContext context;
+
+    private void refreshContext() {
+        context = FacesContext.getCurrentInstance().getExternalContext();
+    }
 
     public boolean isUserInOneOfRoles(String... roles) {
-        FacesContext context = FacesContext.getCurrentInstance();
-        ExternalContext externalContext = context.getExternalContext();
+        refreshContext();
         for (String role : roles) {
-            if (externalContext.isUserInRole(role)) {
+            if (context.isUserInRole(role)) {
                 return true;
             }
         }
@@ -32,45 +34,20 @@ public class RoleController implements Serializable {
     }
 
     public String[] getAllUserRoles() {
+        refreshContext();
         List<String> roles = new ArrayList<>();
-        FacesContext context = FacesContext.getCurrentInstance();
-        ExternalContext externalContext = context.getExternalContext();
-        if (externalContext.isUserInRole("CLIENT")) {
+        if (context.isUserInRole("CLIENT")) {
+            System.out.println("c");
             roles.add("Client");
         }
-        if (externalContext.isUserInRole("MANAGER")) {
+        if (context.isUserInRole("MANAGER")) {
+            System.out.println("m");
             roles.add("Manager");
         }
-        if (externalContext.isUserInRole("ADMIN")) {
+        if (context.isUserInRole("ADMIN")) {
+            System.out.println("a");
             roles.add("Admin");
         }
-        if (roles.isEmpty()) {
-            roles.add("Guest");
-        }
         return roles.toArray(new String[0]);
-    }
-
-    public void redirectByRole() throws IOException {
-        FacesContext context = FacesContext.getCurrentInstance();
-        ExternalContext externalContext = context.getExternalContext();
-        switch (selectedRole) {
-            case "Client":
-                externalContext.redirect("client/clientPage.xhtml");
-                break;
-            case "Manager":
-                externalContext.redirect("manager/managerPage.xhtml");
-                break;
-            case "Admin":
-                externalContext.redirect("admin/adminPage.xhtml");
-                break;
-            default:
-                externalContext.redirect("index.xhtml");
-                break;
-        }
-    }
-
-    @PostConstruct
-    public void setPrimaryRole() {
-        selectedRole = getAllUserRoles()[0];
     }
 }
