338d05c8cbefddc3ef801b0f1f6afe0962ea0b71
195460 Kowalski Krystian <195460@edu.p.lodz.pl>
2020-03-30 12:40:46 +0200

    "Wstrzyknięcie" RoleControllera do LogoutControllera

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/controllers/authentication/LogoutController.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/controllers/authentication/LogoutController.java
@@ -3,6 +3,7 @@ package pl.lodz.p.it.ssbd2020.ssbd05.mok.controllers.authentication;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.IOException;
 
@@ -10,8 +11,10 @@ import java.io.IOException;
 @Named
 public class LogoutController {
 
+    @Inject
+    RoleController roleController;
+
     public void logout() throws IOException {
-        RoleController roleController = new RoleController();
         roleController.setSelectedRole(null);
         ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
         externalContext.invalidateSession();
