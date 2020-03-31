1ec98824e5118209e8c584437d61bfa5b3484323
Jakub Bogdan <kubab1998@o2.pl>
2020-03-31 16:59:03 +0200

    Wygenerowane encje MOS

---
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mos/entities/Hall.java
@@ -0,0 +1,142 @@
+package pl.lodz.p.it.ssbd2020.ssbd05.mos.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "hall")
+@NamedQueries({
+    @NamedQuery(name = "Hall.findAll", query = "SELECT h FROM Hall h"),
+    @NamedQuery(name = "Hall.findById", query = "SELECT h FROM Hall h WHERE h.id = :id"),
+    @NamedQuery(name = "Hall.findByName", query = "SELECT h FROM Hall h WHERE h.name = :name"),
+    @NamedQuery(name = "Hall.findByCapacity", query = "SELECT h FROM Hall h WHERE h.capacity = :capacity"),
+    @NamedQuery(name = "Hall.findByAddress", query = "SELECT h FROM Hall h WHERE h.address = :address"),
+    @NamedQuery(name = "Hall.findByActive", query = "SELECT h FROM Hall h WHERE h.active = :active"),
+    @NamedQuery(name = "Hall.findByArea", query = "SELECT h FROM Hall h WHERE h.area = :area"),
+    @NamedQuery(name = "Hall.findByDescription", query = "SELECT h FROM Hall h WHERE h.description = :description"),
+    @NamedQuery(name = "Hall.findByPrice", query = "SELECT h FROM Hall h WHERE h.price = :price"),
+    @NamedQuery(name = "Hall.findByVersion", query = "SELECT h FROM Hall h WHERE h.version = :version")})
+public class Hall implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 32)
+    @Column(name = "name", length = 32, nullable = false)
+    private String name;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "capacity", nullable = false)
+    private int capacity;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 32)
+    @Column(name = "address", length = 32, nullable = false)
+    private String address;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "active", nullable = false)
+    private boolean active;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "area", nullable = false)
+    private double area;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 2147483647)
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "price", nullable = false)
+    private double price;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "version", nullable = false, columnDefinition = "bigint default 1")
+    private long version;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "typeId")
+    private Collection<EventTypes> eventTypesCollection;
+
+    public Hall() {
+    }
+
+    public Hall(Long id) {
+        this.id = id;
+    }
+
+    public Hall(Long id, String name, int capacity, String address, boolean active, double area, String description, double price, long version) {
+        this.id = id;
+        this.name = name;
+        this.capacity = capacity;
+        this.address = address;
+        this.active = active;
+        this.area = area;
+        this.description = description;
+        this.price = price;
+        this.version = version;
+    }
+
+    public Collection<EventTypes> getEventTypesCollection() {
+        return eventTypesCollection;
+    }
+
+    public void setEventTypesCollection(Collection<EventTypes> eventTypesCollection) {
+        this.eventTypesCollection = eventTypesCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Hall)) {
+            return false;
+        }
+        Hall other = (Hall) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "pl.lodz.p.it.ssbd2020.ssbd05.mos.entities.Hall[ id=" + id + " ]";
+    }
+
+}
