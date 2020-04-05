1cf35d16316685eceb41ad2f34ece5708049c263
216808 Kowalski Krystian <216808@edu.p.lodz.pl>
2020-04-05 16:43:57 +0200

    Dodanie klasy encji oraz fasady dla ExtraServiceMapping

---
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mor/facades/ExtraServiceMappingFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.ssbd2020.ssbd05.mor.facades;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import pl.lodz.p.it.ssbd2020.ssbd05.AbstractFacade;
+import pl.lodz.p.it.ssbd2020.ssbd05.mor.entities.ExtraServiceMapping;
+
+
+@Stateless
+public class ExtraServiceMappingFacade extends AbstractFacade<ExtraServiceMapping> {
+
+    @PersistenceContext(unitName = "ssbd05mokPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ExtraServiceMappingFacade() {
+        super(ExtraServiceMapping.class);
+    }
+    
+}
