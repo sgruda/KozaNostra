9de9d7edbe63be790191df36534ebb04dd277a5f
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-04-06 19:09:28 +0200

    Dodanie wyświetlania szczegółów kont

---
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/controllers/AccountDetailsController.java
@@ -0,0 +1,34 @@
+package pl.lodz.p.it.ssbd2020.ssbd05.mok.controllers;
+
+import lombok.Getter;
+import pl.lodz.p.it.ssbd2020.ssbd05.mok.entities.Account;
+
+import javax.annotation.PreDestroy;
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ConversationScoped
+@RolesAllowed(value = "ADMIN")
+public class AccountDetailsController implements Serializable {
+
+    @Inject
+    private Conversation conversation;
+    @Getter
+    private Account account; //docelowo AccountDTO
+
+    public String selectAccount(Account account) {
+        conversation.begin();
+        this.account = account;
+        return "accountDetails";
+    }
+
+    @PreDestroy
+    public void end() {
+        conversation.end();
+    }
+}
