e202cac013440be63de418d28b3660ef4bf1ae6a
Szymon Gruda <216773@edu.p.lodz.pl>
2020-04-10 15:28:01 +0200

    Dodanie zapisu do bazy danych o ostatnim logowaniu

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/web/auth/LoginController.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/web/auth/LoginController.java
@@ -2,8 +2,12 @@ package pl.lodz.p.it.ssbd2020.ssbd05.web.auth;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import pl.lodz.p.it.ssbd2020.ssbd05.entities.mok.Account;
+import pl.lodz.p.it.ssbd2020.ssbd05.mok.facades.AccountFacade;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
@@ -13,10 +17,20 @@ import javax.inject.Named;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
 @Named
 @ViewScoped
+@Slf4j
 public class LoginController implements Serializable {
 
     @Inject
@@ -26,6 +40,11 @@ public class LoginController implements Serializable {
     @Getter @Setter
     private String password;
     private String originalUrl;
+
+    @EJB
+    private AccountFacade accountFacade; //docelowo AccountService
+    @Getter
+    private Account account;
 
     @PostConstruct
     public void init() {
@@ -48,24 +67,59 @@ public class LoginController implements Serializable {
         ExternalContext externalContext = context.getExternalContext();
         HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
 
+        this.account = accountFacade.findByLogin(username);
+        //TODO A co z wyjatkiem? jak nie znajdzie? Jakis catch by sie przydal
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("lastSuccesfullAuthDate", account.getLastSuccessfulAuth());
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("lastFailedAuthDate", account.getLastFailedAuth());
         try {
             request.login(username, password);
             roleController.setSelectedRole(roleController.getAllUserRoles()[0]);
             externalContext.redirect(originalUrl);
             externalContext.getSessionMap().put("printLastLoginInfo", true);
-
+            this.updateLastSuccesfullAuthDate();
         } catch (ServletException e) {
             context.addMessage(null, new FacesMessage("Incorrect credentials"));
+            this.updateLastFailedAuthDate();
         }
+        this.updateLastAuthIP();
+        this.accountFacade.edit(this.account);
     }
     public void informAboutLastAuthentication() {
         FacesContext context = FacesContext.getCurrentInstance();
         ExternalContext externalContext = context.getExternalContext();
         boolean printLastLoginInfo = (boolean) externalContext.getSessionMap().getOrDefault("printLastLoginInfo", false);
         if (printLastLoginInfo) {
-            context.addMessage(null, new FacesMessage("Successful",  "Last correct authentication..." ) );
-            context.addMessage(null, new FacesMessage("Successful",  "Last incorrect authentication..." ) );
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Last correct authentication",  externalContext.getSessionMap().getOrDefault("lastSuccesfullAuthDate", "Default").toString() ) );
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Last incorrect authentication",  externalContext.getSessionMap().getOrDefault("lastFailedAuthDate", "Default").toString() ) );
             externalContext.getSessionMap().remove("printLastLoginInfo");
+            externalContext.getSessionMap().remove("lastSuccesfullAuthDate");
+            externalContext.getSessionMap().remove("lastFailedAuthDate");
         }
+    }
+    private void updateLastAuthIP() {
+        account.setLastAuthIp(getIP());
+    }
+    private void updateLastSuccesfullAuthDate() {
+        account.setLastSuccessfulAuth(Date.from(Instant.now()));
+        account.setFailedAuthCounter(0);
+    }
+    private void updateLastFailedAuthDate() {
+        account.setLastFailedAuth(Date.from(Instant.now()));
+        account.setFailedAuthCounter(account.getFailedAuthCounter() + 1);
+    }
+    private String getIP() {
+        URL urlToCheckIpAmazonaws;
+        String ipAddress = "";
+        try {
+            urlToCheckIpAmazonaws = new URL("http://checkip.amazonaws.com");
+            try(BufferedReader in = new BufferedReader(new InputStreamReader(urlToCheckIpAmazonaws.openStream()))) {
+                ipAddress = in.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return ipAddress;
     }
 }
