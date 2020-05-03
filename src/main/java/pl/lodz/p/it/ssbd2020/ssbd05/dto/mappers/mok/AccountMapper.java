374342c301943ec1f3affbd82d710357a0580bd5
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-05-03 23:54:31 +0200

    Dodanie mappera pomiędzy Account i AccountDTO korzystającego z biblioteki mapstruct

---
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/dto/mappers/mok/AccountMapper.java
@@ -0,0 +1,34 @@
+package pl.lodz.p.it.ssbd2020.ssbd05.dto.mappers.mok;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+import pl.lodz.p.it.ssbd2020.ssbd05.dto.mok.AccountDTO;
+import pl.lodz.p.it.ssbd2020.ssbd05.entities.mok.AccessLevel;
+import pl.lodz.p.it.ssbd2020.ssbd05.entities.mok.Account;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Mapper
+public interface AccountMapper {
+
+    AccountMapper INSTANCE = Mappers.getMapper(AccountMapper.class);
+
+    AccountDTO toAccountDTO(Account Account); //do wyswietlania
+    Account createNewAccount(AccountDTO accountDTO); //do tworzenia nowych
+    void updateAccountFromDTO(AccountDTO accountDTO, @MappingTarget Account account); //do update'u, nie ustawia accessLevelCollection
+
+    //metoda uzywana w toAccountDTO
+    default Collection<String> toAccessLevelStringCollection(Collection<AccessLevel> accessLevelCollection) {
+        return accessLevelCollection.stream()
+                .map(AccessLevel::getAccessLevel)
+                .collect(Collectors.toList());
+    }
+
+    //metoda uzywana w createNewAccount i updateAccountFromDTO
+    default Collection<AccessLevel> toAccessLevelCollection(Collection<String> accessLevelStringCollection) {
+        return new ArrayList<>();
+    }
+}
