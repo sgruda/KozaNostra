6b6af008119be31c183e3c6514a2eabfad4f5de3
Szymon Gruda <216773@edu.p.lodz.pl>
2020-05-11 20:01:08 +0200

    Refactoring klasy ResourceBundles

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/utils/ResourceBundles.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/utils/ResourceBundles.java
@@ -1,7 +1,6 @@
 package pl.lodz.p.it.ssbd2020.ssbd05.utils;
 
 import javax.faces.application.FacesMessage;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import java.util.ResourceBundle;
 
@@ -11,32 +10,31 @@ public class ResourceBundles {
     }
 
     public static void emitErrorMessage(final String id, final String key) {
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,getTranslatedText(key),getTranslatedText(key));
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, getTranslatedText(key),getTranslatedText(key));
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
-
-    public static void emitMessage(final String id, final String key) {
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,getTranslatedText(key),getTranslatedText(key));
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-
-    public static void emitMessageWithData(final String id, final String key, ExternalContext externalContext, String key2) {
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,getTranslatedText(key),externalContext.getSessionMap().get(key2).toString() );
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-
-    public static void emitErrorMessageWithData(final String id, final String key, ExternalContext externalContext, String key2) {
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,getTranslatedText(key),externalContext.getSessionMap().get(key2).toString() );
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-    public static void emitDetailedMessageWithFlash(final String id, final String key, final String data) {
+    public static void emitErrorMessageWithFlash(final String id, final String key) {
         FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, getTranslatedText(key), data);
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, getTranslatedText(key),getTranslatedText(key));
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
     public static void emitDetailedErrorWithFlash(final String id, final String key, final String data) {
         FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
         FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, getTranslatedText(key), data);
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    public static void emitMessage(final String id, final String key) {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, getTranslatedText(key),getTranslatedText(key));
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    public static void emitMessageWithFlash(final String id, final String key) {
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, getTranslatedText(key),getTranslatedText(key));
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    public static void emitDetailedMessageWithFlash(final String id, final String key, final String data) {
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, getTranslatedText(key), data);
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 }
