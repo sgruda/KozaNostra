d8cae17e06082574a2e69d36290dca0494061a50
216848 Gabriel Nowak <216848@edu.p.lodz.pl>
2020-05-13 00:09:40 +0200

    Szczegóły własnego konta przerzucone do managera, Usunięcie rolesAllowed z kontrolerów

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/endpoints/AccountDetailsEndpoint.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/endpoints/AccountDetailsEndpoint.java
@@ -10,6 +10,7 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateful;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -26,6 +27,13 @@ public class AccountDetailsEndpoint implements Serializable {
 
     @RolesAllowed("getOtherAccount")
     public AccountDTO getAccount(String login) {
+        this.account = accountManager.findByLogin(login);
+        return AccountMapper.INSTANCE.toAccountDTO(account);
+    }
+
+    @RolesAllowed("getOwnAccount")
+    public AccountDTO getOwnAccount() {
+        String login = FacesContext.getCurrentInstance().getExternalContext().getRemoteUser();
         this.account = accountManager.findByLogin(login);
         return AccountMapper.INSTANCE.toAccountDTO(account);
     }
