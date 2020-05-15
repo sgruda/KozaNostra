db7e8c80876455a943f94e32390ab679482499ae
216728 Jakub Bogdan <216728@edu.p.lodz.pl>
2020-05-15 16:53:30 +0200

    Dodane rzucanie wyjątków

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/abstraction/AbstractManager.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/abstraction/AbstractManager.java
@@ -3,6 +3,8 @@ package pl.lodz.p.it.ssbd2020.ssbd05.abstraction;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.ejb.EJBException;
+import java.rmi.RemoteException;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
@@ -12,16 +14,16 @@ abstract public class AbstractManager  {
     private boolean lastTransactionRollback;
     private String transactionId;
 
-    public void afterBegin() {
+    public void afterBegin() throws EJBException, RemoteException {
         transactionId = Long.toString(System.currentTimeMillis()) + ThreadLocalRandom.current().nextLong(Long.MAX_VALUE);
         log.info("Transaction with ID: " + transactionId + " has been started");
     }
 
-    public void beforeCompletion() {
+    public void beforeCompletion() throws EJBException, RemoteException {
         log.info("Transaction with ID: " + transactionId + " before completion");
     }
 
-    public void afterCompletion(boolean committed) {
+    public void afterCompletion(boolean committed) throws EJBException, RemoteException {
         lastTransactionRollback = !committed;
         log.info("Transaction with ID: " + transactionId + " has been completed by: " + (committed?"commit":"rollback"));
     }
