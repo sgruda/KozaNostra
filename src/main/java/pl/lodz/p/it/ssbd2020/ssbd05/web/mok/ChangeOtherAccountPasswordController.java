5ed9511bc96ba3270ce526051b469830f7a9e4c4
Szymon Rutkowski <s.rutkowski235@gmail.com>
2020-05-17 15:41:22 +0200

    implementacja kontrolera oraz widoku

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/web/mok/ChangeOtherAccountPasswordController.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/web/mok/ChangeOtherAccountPasswordController.java
@@ -5,13 +5,19 @@ import lombok.Setter;
 import lombok.extern.java.Log;
 import pl.lodz.p.it.ssbd2020.ssbd05.dto.mok.AccountDTO;
 import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.AppBaseException;
+import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.io.database.AppOptimisticLockException;
+import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.mok.AccountPasswordAlreadyUsedException;
 import pl.lodz.p.it.ssbd2020.ssbd05.mok.endpoints.EditAccountEndpoint;
 import pl.lodz.p.it.ssbd2020.ssbd05.mok.endpoints.interfaces.EditAccountEndpointLocal;
+import pl.lodz.p.it.ssbd2020.ssbd05.utils.HashGenerator;
+import pl.lodz.p.it.ssbd2020.ssbd05.utils.ResourceBundles;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -32,15 +38,39 @@ public class ChangeOtherAccountPasswordController implements Serializable {
     private String previousPassword;
     @Getter
     @Setter
+    private String newPassword;
+    @Getter
+    @Setter
+    private String newConfirmPassword;
+    @Getter
+    @Setter
     private AccountDTO accountDTO;
 
 
     public void init(String login) throws AppBaseException{
+        if(!conversation.isTransient()) {
+            conversation.end();
+        }
         conversation.begin();
         this.accountDTO=editAccountEndpointLocal.findByLogin(login);
-        log.fine(this.accountDTO.getLogin() +" " + this.accountDTO.getFirstname());
+        log.severe("siema" + this.accountDTO.getLogin() + " " + this.accountDTO.getFirstname());
 
     }
+
+    public void setPassword() throws AppBaseException {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        AccountDTO accountDTO = editAccountEndpointLocal.findByLogin(context.getRemoteUser());
+
+        try {
+            editAccountEndpointLocal.changePassword(newPassword, accountDTO);
+            ResourceBundles.emitMessage(null, "page.changepassword.message");
+        } catch (AppOptimisticLockException ex) {
+            ResourceBundles.emitErrorMessage(null, ex.getMessage());
+        } catch (AccountPasswordAlreadyUsedException ex) {
+            ResourceBundles.emitErrorMessage(null, ex.getMessage());
+        }
+    }
+
 
     @RolesAllowed(value = "ADMIN")
     public String redirectToChangePassword() {
