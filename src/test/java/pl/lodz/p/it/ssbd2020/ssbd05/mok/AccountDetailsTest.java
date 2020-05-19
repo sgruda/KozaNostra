15800775362c3682fc29719402ee918be2812149
216728 Jakub Bogdan <216728@edu.p.lodz.pl>
2020-05-19 22:46:05 +0200

    Przygotowanie do testów Selenium

---
+++ src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/AccountDetailsTest.java
@@ -0,0 +1,57 @@
+package pl.lodz.p.it.ssbd2020.ssbd05.mok;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class AccountDetailsTest {
+
+    private WebDriver driver;
+    JavascriptExecutor js;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/drivers/geckodriver.exe");
+        FirefoxOptions options = new FirefoxOptions();
+//        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        driver = new FirefoxDriver();
+//        driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void accountDetailsTest() {
+        driver.get("https://localhost:8181/ssbd05/index.xhtml");
+        driver.manage().window().setSize(new Dimension(1552, 840));
+        driver.findElement(By.id("loginButton")).click();
+        driver.findElement(By.id("login:username")).click();
+        driver.findElement(By.id("login:username")).sendKeys("admin");
+        driver.findElement(By.id("login:password")).sendKeys("admin123");
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.findElement(By.cssSelector(".ui-button-text:nth-child(2)")).click();
+        driver.findElement(By.cssSelector(".ui-button-text:nth-child(2)")).click();
+        driver.findElement(By.cssSelector(".ui-state-hover > .ui-menuitem-text")).click();
+        driver.findElement(By.cssSelector("#j_idt26\\3A accountDetailsButton > .ui-menuitem-text")).click();
+        driver.findElement(By.cssSelector(".ui-button-text:nth-child(2)")).click();
+
+        String login = (String) js.executeScript("document.getElementById('loginDetails').value");
+        assertEquals("admin", login);
+
+        driver.findElement(By.cssSelector(".ui-state-hover > .ui-menuitem-text")).click();
+    }
+}
+
