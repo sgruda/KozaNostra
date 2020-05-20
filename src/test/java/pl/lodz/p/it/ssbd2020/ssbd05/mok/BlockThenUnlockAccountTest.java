3852a9203eba60ff90267c8682eacd0792a08ce2
Szymon Rutkowski <s.rutkowski235@gmail.com>
2020-05-20 21:19:01 +0200

    Test selenium do blokowania i odblokowania konta

---
+++ src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/BlockThenUnlockAccountTest.java
@@ -0,0 +1,59 @@
+package pl.lodz.p.it.ssbd2020.ssbd05.mok;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+public class BlockThenUnlockAccountTest {
+    private WebDriver driver;
+    JavascriptExecutor js;
+
+
+    @Before
+    public void setUp() {
+        WebDriverManager.getInstance(ChromeDriver.class).setup();
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        options.setAcceptInsecureCerts(true);
+        driver = new ChromeDriver(options);
+        js = (JavascriptExecutor) driver;
+
+    }
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+    @Test
+    public void blockThenUnlockAccount() {
+        driver.get("https://localhost:8181/ssbd05/index.xhtml");
+        driver.manage().window().setSize(new Dimension(1552, 840));
+        driver.findElement(By.id("loginButton")).click();
+        driver.findElement(By.id("login:username")).click();
+        driver.findElement(By.id("login:username")).sendKeys("admin");
+        driver.findElement(By.id("login:password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//*[contains(@id, 'submit')]")).click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//*[contains(@id, 'dynaButton')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'changeRole')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'changeForAdmin')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'menu-button')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'listAccountsButton')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'accountfilter')]")).sendKeys("Szajmi2");
+        driver.findElement(By.xpath("//*[contains(@id, 'filterbutton')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'goDetailsButton')]")).click();
+        String aktywny = driver.findElement(By.xpath("//*[contains(@id, 'aktywny')]")).getText();
+        Assert.assertEquals("Tak",aktywny);
+        driver.findElement(By.xpath("//*[contains(@id, 'blockaccount')]")).click();
+        String nieaktywny = driver.findElement(By.xpath("//*[contains(@id, 'nieaktywny')]")).getText();
+        Assert.assertEquals("Nie",nieaktywny);
+        driver.findElement(By.xpath("//*[contains(@id, 'unlockaccount')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'dynaButton')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'logoutButton')]")).click();
+    }
+}
