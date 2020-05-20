1c4140d0f3b0882486af7b599f010c617c361322
216728 Jakub Bogdan <216728@edu.p.lodz.pl>
2020-05-20 22:55:19 +0200

    Dokończone testy

--- src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/AccountDetailsTest.java
+++ src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/AccountDetailsTest.java
@@ -36,7 +36,6 @@ public class AccountDetailsTest {
     @Test
     public void accountDetailsTest() {
         driver.get("https://localhost:8181/ssbd05/index.xhtml");
-        driver.manage().window().setSize(new Dimension(1552, 840));
         driver.findElement(By.id("loginButton")).click();
         driver.findElement(By.id("login:username")).click();
         driver.findElement(By.id("login:username")).sendKeys("admin");
