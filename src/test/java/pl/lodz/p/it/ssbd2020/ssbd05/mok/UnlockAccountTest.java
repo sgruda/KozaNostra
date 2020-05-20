28009e15fdb6a916f0cd40fe9c45b04cc820879b
Szymon Rutkowski <s.rutkowski235@gmail.com>
2020-05-20 20:58:57 +0200

    Test odblokowania konta działający w junit

--- src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/UnlockAccountTest.java
+++ src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/UnlockAccountTest.java
@@ -46,42 +46,76 @@ public class UnlockAccountTest {
     }
     @Test
     public void unlockAccount() {
-        driver.get("https://localhost:8181/ssbd05/admin/listAccounts.xhtml");
-        driver.manage().window().setSize(new Dimension(1528, 814));
+//        driver.get("https://localhost:8181/ssbd05/admin/listAccounts.xhtml");
+//        driver.manage().window().setSize(new Dimension(1528, 814));
+//        log.severe("przed logowaniem " +driver.getCurrentUrl());
+//        driver.findElement(By.id("login:username")).click();
+//        driver.findElement(By.id("login:username")).sendKeys("admin");
+//        driver.findElement(By.id("login:password")).sendKeys("admin123");
+//        driver.findElement(By.cssSelector(".ui-button-text:nth-child(2)")).click();
+//        driver.findElement(By.id("form1:j_idt41")).click();
+//        driver.findElement(By.id("form1:j_idt41")).sendKeys("Szajmi2");
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//        driver.findElement(By.cssSelector("#form1\\3A filterbutton > .ui-button-text")).click();
+////        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+////        driver.findElement(By.cssSelector("#form1\\3Aj_idt42\\3A 0\\3Aj_idt56 > .ui-button-text")).click();
+////        driver.findElement(By.name("form1:j_idt42:0:j_idt56")).click();
+//        log.severe(driver.getCurrentUrl().toString());
+//        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+//        while(staleElement) {
+////        driver.findElement(By.name("commandButtons:unlockaccount"));
+//            try {
+//                driver.findElement(By.xpath("//*[contains(@id,'details')]")).click();
+//                staleElement=false;
+//            }catch (StaleElementReferenceException staleElementReferenceException) {
+//                staleElement = true;
+//            }
+//        }
+//        log.severe("po wcisnieciu details: " +driver.getCurrentUrl().toString());
+//        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+//        while(staleElement) {
+//            try {
+//                driver.findElement(By.xpath("//*[contains(@id,'unlockaccount')]")).click();
+//                staleElement=false;
+//            }catch (StaleElementReferenceException staleElementReferenceException){
+//                staleElement=true;
+//            }
+//        }
+//        log.severe("koniec: " + driver.getCurrentUrl());
+////      driver.findElement(By.xpath("//*[contains(@id,'changeAdmin')]")).click();
+////        driver.findElement(By.cssSelector("#commandButtons\\3Aj_idt71 > .ui-button-text")).click();
+        driver.get("https://localhost:8181/ssbd05/index.xhtml");
+        driver.manage().window().setSize(new Dimension(1552, 840));
+        log.severe(driver.getCurrentUrl());
+        driver.findElement(By.id("loginButton")).click();
         driver.findElement(By.id("login:username")).click();
         driver.findElement(By.id("login:username")).sendKeys("admin");
         driver.findElement(By.id("login:password")).sendKeys("admin123");
-        driver.findElement(By.cssSelector(".ui-button-text:nth-child(2)")).click();
-        driver.findElement(By.id("form1:j_idt41")).click();
-        driver.findElement(By.id("form1:j_idt41")).sendKeys("Szajmi2");
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.findElement(By.cssSelector("#form1\\3A filterbutton > .ui-button-text")).click();
-//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//        driver.findElement(By.cssSelector("#form1\\3Aj_idt42\\3A 0\\3Aj_idt56 > .ui-button-text")).click();
-//        driver.findElement(By.name("form1:j_idt42:0:j_idt56")).click();
-        log.severe(driver.getCurrentUrl().toString());
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        while(staleElement) {
-//        driver.findElement(By.name("commandButtons:unlockaccount"));
-            try {
-                driver.findElement(By.xpath("//*[contains(@id,'details')]")).click();
-                staleElement=false;
-            }catch (StaleElementReferenceException staleElementReferenceException) {
-                staleElement = true;
-            }
-        }
-        log.severe("po wcisnieciu details: " +driver.getCurrentUrl().toString());
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        while(staleElement) {
-            try {
-                driver.findElement(By.xpath("//*[contains(@id,'unlockaccount')]")).click();
-                staleElement=false;
-            }catch (StaleElementReferenceException staleElementReferenceException){
-                staleElement=true;
-            }
-        }
-        log.severe("koniec: " + driver.getCurrentUrl());
-//      driver.findElement(By.xpath("//*[contains(@id,'changeAdmin')]")).click();
-//        driver.findElement(By.cssSelector("#commandButtons\\3Aj_idt71 > .ui-button-text")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'submit')]")).click();
+        log.severe(driver.getCurrentUrl());
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//*[contains(@id, 'dynaButton')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'changeRole')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'changeForAdmin')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'menu-button')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'listAccountsButton')]")).click();
+        log.severe(driver.getCurrentUrl());
+//        driver.findElement(By.id("form1:j_idt35")).click();
+//        driver.findElement(By.id("form1:j_idt35")).sendKeys("Szajmi");
+//        driver.findElement(By.cssSelector("#form1\\3A filterbutton > .ui-button-text")).click();
+//        driver.findElement(By.xpath("//*[contains(@id, 'accountfilter')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'accountfilter')]")).sendKeys("Szajmi2");
+        driver.findElement(By.xpath("//*[contains(@id, 'filterbutton')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'goDetailsButton')]")).click();
+        log.severe("po details: " + driver.getCurrentUrl());
+        driver.findElement(By.xpath("//*[contains(@id, 'blockaccount')]")).click();
+        log.severe("po unlocku: " + driver.getCurrentUrl());
+//        String login = driver.findElement(By.xpath("//*[contains(@id, 'loginDetails')]")).getText();
+//        String email = driver.findElement(By.xpath("//*[contains(@id, 'emailDetails')]")).getText();
+//        assertEquals("admin", login);
+//        assertEquals("sanah@example.com", email);
+
+        driver.findElement(By.xpath("//*[contains(@id, 'dynaButton')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'logoutButton')]")).click();
     }
 }
