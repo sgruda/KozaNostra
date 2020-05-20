b41ffb3f2b25916cc0468db4fea1d66ea72a7391
216728 Jakub Bogdan <216728@edu.p.lodz.pl>
2020-05-20 16:22:34 +0200

    Sterownik pobierany za pomocą WebDriverManager

--- src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/AccountDetailsTest.java
+++ src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/AccountDetailsTest.java
@@ -1,5 +1,6 @@
 package pl.lodz.p.it.ssbd2020.ssbd05.mok;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
@@ -8,8 +9,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import java.util.concurrent.TimeUnit;
@@ -21,7 +20,7 @@ public class AccountDetailsTest {
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/drivers/chromedriver.exe");
+        WebDriverManager.getInstance(ChromeDriver.class).setup();
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
         driver = new ChromeDriver();
@@ -36,6 +35,21 @@ public class AccountDetailsTest {
     @Test
     public void accountDetailsTest() {
         driver.get("https://localhost:8181/ssbd05/index.xhtml");
-
+//        driver.manage().window().setSize(new Dimension(1552, 840));
+//        driver.findElement(By.id("loginButton")).click();
+//        driver.findElement(By.id("login:username")).click();
+//        driver.findElement(By.id("login:username")).sendKeys("admin");
+//        driver.findElement(By.id("login:password")).sendKeys("admin123");
+//        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+//        driver.findElement(By.cssSelector(".ui-button-text:nth-child(2)")).click();
+//        driver.findElement(By.cssSelector(".ui-button-text:nth-child(2)")).click();
+//        driver.findElement(By.cssSelector(".ui-state-hover > .ui-menuitem-text")).click();
+//        driver.findElement(By.cssSelector("#j_idt26\\3A accountDetailsButton > .ui-menuitem-text")).click();
+//        driver.findElement(By.cssSelector(".ui-button-text:nth-child(2)")).click();
+//
+//        String login = (String) js.executeScript("document.getElementById('loginDetails').value");
+//        assertEquals("admin", login);
+//
+//        driver.findElement(By.cssSelector(".ui-state-hover > .ui-menuitem-text")).click();
     }
 }
