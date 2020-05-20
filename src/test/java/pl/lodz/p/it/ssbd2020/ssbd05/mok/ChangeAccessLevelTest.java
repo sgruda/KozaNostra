2b9aeaaecd4cd6486eb18a8efd0334f843882429
Szymon Gruda <216773@edu.p.lodz.pl>
2020-05-20 21:22:23 +0200

    Dodanie testowania zmiany poziomu dostepu

--- src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/ChangeAccessLevelTest.java
+++ src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/ChangeAccessLevelTest.java
@@ -1,21 +1,17 @@
 package pl.lodz.p.it.ssbd2020.ssbd05.mok;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+
 
 public class ChangeAccessLevelTest {
     private WebDriver driver;
-    private JavascriptExecutor js;
 
     @Before
     public void setUp() {
@@ -24,7 +20,6 @@ public class ChangeAccessLevelTest {
         options.setHeadless(true);
         options.setAcceptInsecureCerts(true);
         driver = new ChromeDriver(options);
-        js = (JavascriptExecutor) driver;
     }
 
     @After
@@ -43,24 +38,24 @@ public class ChangeAccessLevelTest {
         driver.findElement(By.id("login:password")).clear();
         driver.findElement(By.id("login:password")).sendKeys("admin123");
         driver.findElement(By.xpath("//button[@id='login:submit']/span[2]")).click();
-        driver.findElement(By.xpath("//button[@id='j_idt26:dynaButton']/span[2]")).click();
-        driver.findElement(By.linkText("Zmień rolę")).click();
-        driver.findElement(By.xpath("//div[@id='j_idt26:j_idt27']/div/div[2]")).click();
+        driver.findElement(By.xpath("//button[@id='j_idt26:dynaButton']/span")).click();
         assertEquals("Klient", driver.findElement(By.xpath("//div[@id='j_idt26:j_idt27']/div/div/ul/li[2]")).getText());
-        driver.findElement(By.xpath("//div[@id='j_idt26:j_idt27']/div/div/ul/li[3]/a/span[2]")).click();
-        driver.findElement(By.xpath("//div[@id='j_idt26:j_idt27']/div/div/ul/li[3]/ul/li[2]/a/span")).click();
-        driver.findElement(By.xpath("//button[@id='j_idt26:dynaButton']/span[2]")).click();
-        assertEquals("Menadżer", driver.findElement(By.xpath("//div[@id='j_idt26:j_idt27']/div/div/ul/li[2]")).getText());
+        driver.findElement(By.xpath("//li[@id='j_idt26:changeRoleButton']/a/span[2]")).click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[@id='j_idt26:changeManager']/span")).click();
         driver.findElement(By.xpath("//a[@id='menu-button']/i")).click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         assertEquals("Panel menadżera", driver.findElement(By.xpath("//div[@id='j_idt9']/h1")).getText());
         driver.findElement(By.xpath("//button[@id='j_idt26:dynaButton']/span[2]")).click();
-        driver.findElement(By.xpath("//div[@id='j_idt26:j_idt27']/div/div/ul/li[3]/a/span[2]")).click();
-        driver.findElement(By.xpath("//div[@id='j_idt26:j_idt27']/div/div/ul/li[3]/ul/li[3]/a/span")).click();
-        driver.findElement(By.xpath("//button[@id='j_idt26:dynaButton']/span[2]")).click();
-        assertEquals("Administrator", driver.findElement(By.xpath("//div[@id='j_idt26:j_idt27']/div/div/ul/li[2]")).getText());
+        assertEquals("Menadżer", driver.findElement(By.xpath("//div[@id='j_idt26:j_idt27']/div/div/ul/li[2]")).getText());
+        driver.findElement(By.xpath("//li[@id='j_idt26:changeRoleButton']/a/span[2]")).click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[@id='j_idt26:changeAdmin']/span")).click();
         driver.findElement(By.xpath("//a[@id='menu-button']/i")).click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         assertEquals("Panel administratora", driver.findElement(By.xpath("//div[@id='j_idt9']/h1")).getText());
         driver.findElement(By.xpath("//button[@id='j_idt26:dynaButton']/span[2]")).click();
+        assertEquals("Administrator", driver.findElement(By.xpath("//div[@id='j_idt26:j_idt27']/div/div/ul/li[2]")).getText());
         driver.findElement(By.xpath("//div[@id='j_idt26:j_idt27']/div/div/ul/li[4]/a/span[2]")).click();
     }
 
