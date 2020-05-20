b58d706d7103fb4e622c97aee0c99c9fdbe7cee2
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-05-20 20:46:21 +0200

    Skończony test na filtrowanie kont, dodanie wylogowania do testów

--- src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/ListAccountsTest.java
+++ src/test/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/ListAccountsTest.java
@@ -1,20 +1,25 @@
 package pl.lodz.p.it.ssbd2020.ssbd05.mok;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import lombok.extern.java.Log;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-@Log
 public class ListAccountsTest {
 
     private WebDriver driver;
@@ -38,24 +43,60 @@ public class ListAccountsTest {
     @Test
     public void getAllAccountsTest() {
         driver.get("https://localhost:8181/ssbd05/index.xhtml");
-        driver.manage().window().setSize(new Dimension(1200, 833));
         driver.findElement(By.id("loginButton")).click();
         driver.findElement(By.id("login:username")).click();
         driver.findElement(By.id("login:username")).sendKeys("admin");
         driver.findElement(By.id("login:password")).sendKeys("admin123");
         driver.findElement(By.xpath("//*[contains(@id,'submit')]")).click();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+
         driver.findElement(By.xpath("//*[contains(@id,'dynaButton')]")).click();
         driver.findElement(By.xpath("//*[contains(@id,'changeRoleButton')]")).click();
         driver.findElement(By.xpath("//*[contains(@id,'changeAdmin')]")).click();
         driver.findElement(By.cssSelector(".pi-bars")).click();
-        driver.findElement(By.cssSelector("#j_idt11 > .ui-button-text")).click();
+        driver.findElement(By.xpath("//*[contains(@id,'listAccountsButton')]")).click();
         List<WebElement> rows = driver.findElements(By.tagName("tr"));
-        assertTrue(rows.size() > 30);
+        assertTrue(rows.size() > 3);
+
+        driver.findElement(By.xpath("//*[contains(@id,'dynaButton')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id,'logoutButton')]")).click();
     }
 
     @Test
     public void filterAccountsTest() {
+        driver.get("https://localhost:8181/ssbd05/index.xhtml");
+        driver.findElement(By.id("loginButton")).click();
+        driver.findElement(By.id("login:username")).click();
+        driver.findElement(By.id("login:username")).sendKeys("admin");
+        driver.findElement(By.id("login:password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//*[contains(@id,'submit')]")).click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 
+        driver.findElement(By.xpath("//*[contains(@id,'dynaButton')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id,'changeRoleButton')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id,'changeAdmin')]")).click();
+        driver.findElement(By.cssSelector(".pi-bars")).click();
+        {
+            WebElement element = driver.findElement(By.cssSelector(".pi-bars"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).perform();
+        }
+        {
+            WebElement element = driver.findElement(By.tagName("body"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element, 0, 0).perform();
+        }
+        driver.findElement(By.xpath("//*[contains(@id,'listAccountsButton')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id,'filterAccountsTextBox')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id,'filterAccountsTextBox')]")).sendKeys("selenium");
+        driver.findElement(By.xpath("//*[contains(@id,'filterbutton')]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("/html/body/div[1]/div[3]/form/div/div/table/tbody/tr[3]")));
+        List<WebElement> rows = driver.findElements(By.tagName("tr"));
+        assertEquals(3, rows.size());
+
+        driver.findElement(By.xpath("//*[contains(@id,'dynaButton')]")).click();
+        driver.findElement(By.xpath("//*[contains(@id,'logoutButton')]")).click();
     }
 }
