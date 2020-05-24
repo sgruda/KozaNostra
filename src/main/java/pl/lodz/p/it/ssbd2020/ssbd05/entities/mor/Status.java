cceb6bd04953264321fd27bda62e539f353f1e5d
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-05-24 14:23:16 +0200

    Zwiększenie wersji i usunięcie todo

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/entities/mor/Status.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/entities/mor/Status.java
@@ -58,7 +58,6 @@ public class Status implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Status)) {
             return false;
         }
--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/entities/mos/Hall.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/entities/mos/Hall.java
@@ -107,7 +107,6 @@ public class Hall implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Hall)) {
             return false;
         }
