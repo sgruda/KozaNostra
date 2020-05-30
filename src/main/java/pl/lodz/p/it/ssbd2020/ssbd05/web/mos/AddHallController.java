8bbf7deafc2713ecbf1ba290fac19db3faa46005
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-05-30 21:09:01 +0200

    Rozpoczęte prace nad kontrolerem

---
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/web/mos/AddHallController.java
@@ -0,0 +1,67 @@
+package pl.lodz.p.it.ssbd2020.ssbd05.web.mos;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.java.Log;
+import pl.lodz.p.it.ssbd2020.ssbd05.dto.mos.AddressDTO;
+import pl.lodz.p.it.ssbd2020.ssbd05.dto.mos.HallDTO;
+import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.AppBaseException;
+import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.ValidationException;
+import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.mok.EmailAlreadyExistsException;
+import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.mok.LoginAlreadyExistsException;
+import pl.lodz.p.it.ssbd2020.ssbd05.mos.endpoints.interfaces.AddHallEndpointLocal;
+import pl.lodz.p.it.ssbd2020.ssbd05.utils.ResourceBundles;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.logging.Level;
+
+@Log
+@Getter
+@Setter
+@Named
+@RequestScoped
+public class AddHallController {
+
+    @Inject
+    private AddHallEndpointLocal addHallEndpoint;
+
+    private HallDTO hall;
+    private List<String> eventTypes;
+    private List<AddressDTO> addresses;
+    private boolean shouldAddNewAddress;
+    private AddressDTO newAddress;
+
+    @PostConstruct
+    public void init() {
+        shouldAddNewAddress = false;
+        try {
+            eventTypes = addHallEndpoint.getAllEventTypes();
+            addresses = addHallEndpoint.getAllAddresses();
+        } catch (AppBaseException e) {
+            log.warning(e.getClass().toString() + " " + e.getMessage());
+            ResourceBundles.emitErrorMessageWithFlash(null, e.getMessage());
+        }
+    }
+
+    public void addHall() {
+        try {
+            addHallEndpoint.addHall(hall);
+            ResourceBundles.emitMessageWithFlash(null,"page.registration.account.created");
+        } catch (ValidationException e) {
+            ResourceBundles.emitErrorMessageByPlainText(null, e.getMessage());
+            log.severe(e.getMessage() + ", " + LocalDateTime.now());
+        } catch (AppBaseException ex) {
+            ResourceBundles.emitErrorMessageWithFlash(null, ex.getMessage());
+            log.severe(ex.getMessage() + ", " + LocalDateTime.now());
+        }
+    }
+
+    public String goBack() {
+        return "home";
+    }
+}
