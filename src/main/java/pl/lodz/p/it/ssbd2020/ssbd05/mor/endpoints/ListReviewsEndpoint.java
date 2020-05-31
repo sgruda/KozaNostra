78ff669a322805f5500c1876d6755f9d06466d2a
216848 Gabriel Nowak <216848@edu.p.lodz.pl>
2020-06-01 01:58:21 +0200

    MOR.12 Backend + mapper do DTO z testem

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mor/endpoints/ListReviewsEndpoint.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mor/endpoints/ListReviewsEndpoint.java
@@ -1,18 +1,23 @@
 package pl.lodz.p.it.ssbd2020.ssbd05.mor.endpoints;
 
 import lombok.extern.java.Log;
+import pl.lodz.p.it.ssbd2020.ssbd05.dto.mappers.mor.ReviewMapper;
 import pl.lodz.p.it.ssbd2020.ssbd05.dto.mor.ReviewDTO;
 import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.AppBaseException;
+import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.io.database.ExceededTransactionRetriesException;
 import pl.lodz.p.it.ssbd2020.ssbd05.interceptors.TrackerInterceptor;
 import pl.lodz.p.it.ssbd2020.ssbd05.mor.endpoints.interfaces.ListReviewsEndpointLocal;
 import pl.lodz.p.it.ssbd2020.ssbd05.mor.managers.ReviewManager;
+import pl.lodz.p.it.ssbd2020.ssbd05.utils.ResourceBundles;
 
 import javax.annotation.security.PermitAll;
+import javax.ejb.EJBTransactionRolledbackException;
 import javax.ejb.Stateful;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 import javax.interceptor.Interceptors;
+import java.util.ArrayList;
 import java.util.List;
 
 @Log
@@ -26,6 +31,24 @@ public class ListReviewsEndpoint implements ListReviewsEndpointLocal {
     @Override
     @PermitAll
     public List<ReviewDTO> getAllReviews() throws AppBaseException{
-        throw new UnsupportedOperationException();
+        ArrayList<ReviewDTO> list = new ArrayList<>();
+        int callCounter = 0;
+        boolean rollback;
+        do {
+            try {
+                list = ReviewMapper.INSTANCE.toReviewDTOArrayList(reviewManager.getAllReviews());
+                rollback = reviewManager.isLastTransactionRollback();
+            } catch (EJBTransactionRolledbackException e) {
+                log.warning("EJBTransactionRolledBack");
+                rollback = true;
+            }
+            if(callCounter > 0)
+                log.info("Transaction with ID " + reviewManager.getTransactionId() + " is being repeated for " + callCounter + " time");
+            callCounter++;
+        } while (rollback && callCounter <= ResourceBundles.getTransactionRepeatLimit());
+        if (rollback) {
+            throw new ExceededTransactionRetriesException();
+        }
+        return list;
     }
 }
