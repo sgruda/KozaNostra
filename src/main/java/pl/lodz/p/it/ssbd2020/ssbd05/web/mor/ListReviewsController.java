6f15b8e4e78177dac2aa4df3402d270e15bda362
216848 Gabriel Nowak <216848@edu.p.lodz.pl>
2020-06-02 15:33:30 +0200

    Kontroler i strona listReviews.xhtml

---
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/web/mor/ListReviewsController.java
@@ -0,0 +1,37 @@
+package pl.lodz.p.it.ssbd2020.ssbd05.web.mor;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.java.Log;
+import pl.lodz.p.it.ssbd2020.ssbd05.dto.mor.ReviewDTO;
+import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.AppBaseException;
+import pl.lodz.p.it.ssbd2020.ssbd05.mor.endpoints.interfaces.ListReviewsEndpointLocal;
+import pl.lodz.p.it.ssbd2020.ssbd05.utils.ResourceBundles;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Log
+@Named
+@ViewScoped
+public class ListReviewsController implements Serializable {
+    @Inject
+    private ListReviewsEndpointLocal listReviewsEndpoint;
+    @Getter
+    @Setter
+    private List<ReviewDTO> reviews;
+
+    @PostConstruct
+    public void init(){
+        try {
+            reviews = listReviewsEndpoint.getAllReviews();
+        } catch (AppBaseException e) {
+            log.warning(e.getClass().toString() + " " + e.getMessage());
+            ResourceBundles.emitErrorMessageWithFlash(null, e.getMessage());
+        }
+    }
+}
