dc725e18c1df91ff8fd9329df7427d060ae1d29d
216848 Gabriel Nowak <216848@edu.p.lodz.pl>
2020-06-02 17:22:06 +0200

    Stronicowanie + naprawienie błędów w kontrolerze

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/web/mor/ListReviewsController.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/web/mor/ListReviewsController.java
@@ -24,14 +24,64 @@ public class ListReviewsController implements Serializable {
     @Getter
     @Setter
     private List<ReviewDTO> reviews;
+    @Getter
+    @Setter
+    private int pages;
+    @Getter
+    @Setter
+    private int currentPage = 1;
+    private final int reviewsPerPage = 2;
+    @Getter
+    @Setter
+    private List<ReviewDTO> pageReviews;
 
     @PostConstruct
     public void init(){
         try {
             reviews = listReviewsEndpoint.getAllReviews();
+            pages = reviews.size() / reviewsPerPage;
+            if(reviews.size() % reviewsPerPage > 0){
+                pages++;
+            }
+            updatePageReviews();
         } catch (AppBaseException e) {
             log.warning(e.getClass().toString() + " " + e.getMessage());
             ResourceBundles.emitErrorMessageWithFlash(null, e.getMessage());
         }
+    }
+
+    public void updatePageReviews(){
+        final int firstReview = (currentPage - 1) * reviewsPerPage;
+        final int lastReview = Math.min(((currentPage - 1) * reviewsPerPage) + reviewsPerPage, reviews.size());
+
+        this.pageReviews = reviews.subList(firstReview,lastReview);
+    }
+
+    public void nextPage(){
+        if(this.currentPage < pages){
+            currentPage++;
+        }
+        updatePageReviews();
+    }
+
+    public boolean displayNextPage(){
+        if(currentPage == pages){
+            return false;
+        }
+        return true;
+    }
+
+    public void previousPage(){
+        if(this.currentPage > 1){
+            currentPage--;
+        }
+        updatePageReviews();
+    }
+
+    public boolean displayPreviousPage(){
+        if(currentPage == 1){
+            return false;
+        }
+        return true;
     }
 }
