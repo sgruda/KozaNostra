e64bc9627e90376962b01e74e2645a2317bd58c5
216808 Krystian Kowalski <216808@edu.p.lodz.pl>
2020-06-03 20:33:56 +0200

    Ujednolicenie internacjonalizacji względem reszty przypadków użycia

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/web/mor/ListUsersReservationController.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/web/mor/ListUsersReservationController.java
@@ -3,12 +3,9 @@ package pl.lodz.p.it.ssbd2020.ssbd05.web.mor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.java.Log;
-import pl.lodz.p.it.ssbd2020.ssbd05.dto.mok.AccountDTO;
 import pl.lodz.p.it.ssbd2020.ssbd05.dto.mor.ReservationDTO;
-import pl.lodz.p.it.ssbd2020.ssbd05.entities.mor.Reservation;
 import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.AppBaseException;
 import pl.lodz.p.it.ssbd2020.ssbd05.mor.endpoints.interfaces.ListUserReservationsEndpointLocal;
-import pl.lodz.p.it.ssbd2020.ssbd05.utils.ReservationUtils;
 import pl.lodz.p.it.ssbd2020.ssbd05.utils.ResourceBundles;
 
 import javax.annotation.PostConstruct;
@@ -19,7 +16,6 @@ import javax.inject.Named;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Properties;
 
 
 @Log
@@ -27,8 +23,6 @@ import java.util.Properties;
 @ViewScoped
 public class ListUsersReservationController implements Serializable {
 
-    @Getter
-    private ReservationUtils reservationUtils;
 
     @Inject
     ListUserReservationsEndpointLocal userReservationsEndpointLocal;
@@ -37,10 +31,13 @@ public class ListUsersReservationController implements Serializable {
     @Setter
     List<ReservationDTO> usersReservations;
 
+    @Getter
+    private ResourceBundles resourceBundles;
+
     @PostConstruct
     private void init() {
         try {
-            this.reservationUtils = new ReservationUtils();
+            resourceBundles = new ResourceBundles();
             usersReservations = userReservationsEndpointLocal.getAllUsersReservations(FacesContext.getCurrentInstance().getExternalContext().getRemoteUser());
         } catch (AppBaseException e) {
             log.severe(e.getMessage() + ", " + LocalDateTime.now());
