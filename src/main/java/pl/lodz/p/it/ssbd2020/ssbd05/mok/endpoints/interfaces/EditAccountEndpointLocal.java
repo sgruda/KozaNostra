b107df3f4960851bfd07f631f321d4cf27b4ec0e
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-06-07 16:43:08 +0200

    Javadoc dla mok 11

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/endpoints/interfaces/EditAccountEndpointLocal.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mok/endpoints/interfaces/EditAccountEndpointLocal.java
@@ -5,10 +5,35 @@ import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.AppBaseException;
 
 import javax.ejb.Local;
 
+/**
+ * Interfejs dla punktu dostępowego EditAccountEndpoint wykorzystywanego do edycji kont użytkowników
+ */
 @Local
 public interface EditAccountEndpointLocal {
+
+    /**
+     * Metoda pobierająca konto do jego późniejszej edycji
+     *
+     * @param username Nazwa użytkownika
+     * @return Obiekt typu AccountDTO
+     * @throws AppBaseException Podstawowy wyjątek aplikacyjny
+     */
     AccountDTO findByLogin(String username) throws AppBaseException;
+
+    /**
+     * Metoda odpowiedzialna za zmianę własnego hasła
+     *
+     * @param accountDTO Obiekt typu AccountDTO
+     * @throws AppBaseException Podstawowy wyjątek aplikacyjny
+     */
     void changePassword(AccountDTO accountDTO) throws AppBaseException;
+
+    /**
+     * Metoda odpowiedzialna za zmianę hasła innego użytkownika
+     *
+     * @param accountDTO Obiekt typu AccountDTO
+     * @throws AppBaseException Podstawowy wyjątek aplikacyjny
+     */
     void changeOtherAccountPassword(AccountDTO accountDTO) throws AppBaseException;
     void editAccount(AccountDTO accountDTO) throws AppBaseException;
     void blockAccount(AccountDTO accountDTO) throws AppBaseException;
