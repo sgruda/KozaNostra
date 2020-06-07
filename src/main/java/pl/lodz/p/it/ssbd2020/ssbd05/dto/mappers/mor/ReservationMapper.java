f396e23df3efafac3b411ffcd7a7a4d1d1027211
Szymon Rutkowski <s.rutkowski235@gmail.com>
2020-06-07 20:39:50 +0200

    mor 7 skończony

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/dto/mappers/mor/ReservationMapper.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/dto/mappers/mor/ReservationMapper.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+import static pl.lodz.p.it.ssbd2020.ssbd05.utils.DateFormatter.WITHOUT_SECONDS;
 import static pl.lodz.p.it.ssbd2020.ssbd05.utils.DateFormatter.WITH_SECONDS;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
