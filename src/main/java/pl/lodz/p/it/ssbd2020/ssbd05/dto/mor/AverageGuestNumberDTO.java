4df6054b10e4c201418636e689d42e36f43cb335
216728 Jakub Bogdan <216728@edu.p.lodz.pl>
2020-06-16 21:00:29 +0200

    Backend obsługujący operacje na agregacie

---
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/dto/mor/AverageGuestNumberDTO.java
@@ -0,0 +1,7 @@
+package pl.lodz.p.it.ssbd2020.ssbd05.dto.mor;
+
+public class AverageGuestNumberDTO {
+    private Long guestSum;
+    private Long eventSum;
+    private Long average;
+}
--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mor/facades/AverageGuestNumberFacade.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/mor/facades/AverageGuestNumberFacade.java
@@ -1,5 +1,6 @@
 package pl.lodz.p.it.ssbd2020.ssbd05.mor.facades;
 
+import lombok.extern.java.Log;
 import org.eclipse.persistence.exceptions.DatabaseException;
 import pl.lodz.p.it.ssbd2020.ssbd05.abstraction.AbstractFacade;
 import pl.lodz.p.it.ssbd2020.ssbd05.entities.mor.AverageGuestNumber;
@@ -10,18 +11,20 @@ import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.io.database.DatabaseConnectionExc
 
 import javax.annotation.security.DenyAll;
 import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.interceptor.Interceptors;
-import javax.persistence.EntityManager;
-import javax.persistence.OptimisticLockException;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceException;
+import javax.persistence.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Klasa fasady dla typu AverageGuestNumberFacade
+ */
+@Log
 @TransactionAttribute(TransactionAttributeType.MANDATORY)
 @Stateless
 @LocalBean
@@ -36,12 +39,21 @@ public class AverageGuestNumberFacade extends AbstractFacade<AverageGuestNumber>
         return em;
     }
 
+    /**
+     * Konstruktor bezparametrowy klasy AverageGuestNumberFacade
+     */
     public AverageGuestNumberFacade() {
         super(AverageGuestNumber.class);
     }
 
+    /**
+     * Edytuj agregat
+     *
+     * @param entity obiekty encyjny typu AverageGuestNumber
+     * @throws AppBaseException Podstawowy wyjątek aplikacyjny
+     */
     @Override
-    @DenyAll
+    @RolesAllowed("changeReservationStatus")
     public void edit(AverageGuestNumber entity) throws AppBaseException {
         try {
             super.edit(entity);
@@ -63,6 +75,9 @@ public class AverageGuestNumberFacade extends AbstractFacade<AverageGuestNumber>
     public List<AverageGuestNumber> findAll() throws AppBaseException {
         try {
             return super.findAll();
+        } catch (NoResultException e) {
+            log.severe("An error occurred while loading an aggregate");
+            throw new AppBaseException(e.getMessage());
         } catch (DatabaseException | PersistenceException e) {
             throw new DatabaseConnectionException(e);
         }
