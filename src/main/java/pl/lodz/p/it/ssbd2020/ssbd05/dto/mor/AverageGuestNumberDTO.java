ca579ccda8eae7aa86207344082227ca7b77ec88
216728 Jakub Bogdan <216728@edu.p.lodz.pl>
2020-06-17 15:31:14 +0200

    Wyświetlanie agregatu

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/dto/mor/AverageGuestNumberDTO.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/dto/mor/AverageGuestNumberDTO.java
@@ -1,8 +1,12 @@
 package pl.lodz.p.it.ssbd2020.ssbd05.dto.mor;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AverageGuestNumberDTO {
     private Long guestSum;
     private Long eventSum;
