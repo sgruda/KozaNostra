12ed8b2b1b421c9f6e8fa17e86a197d751fdab65
Szymon Gruda <216773@edu.p.lodz.pl>
2020-06-19 15:03:09 +0200

    Usuniecie nieuzywanych kluczow wyjatkow

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/exceptions/mok/AccountAlreadyConfirmedException.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/exceptions/mok/AccountAlreadyConfirmedException.java
@@ -3,8 +3,6 @@ package pl.lodz.p.it.ssbd2020.ssbd05.exceptions.mok;
 import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.AppBaseException;
 
 public class AccountAlreadyConfirmedException extends AppBaseException {
-    static final public String KEY_CONFIRMED = "error.account.confirmed";
-
     public AccountAlreadyConfirmedException(String message) {
         super(message);
     }
--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/exceptions/mor/ExtraServiceAlreadyExistsException.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/exceptions/mor/ExtraServiceAlreadyExistsException.java
@@ -3,8 +3,6 @@ package pl.lodz.p.it.ssbd2020.ssbd05.exceptions.mor;
 import pl.lodz.p.it.ssbd2020.ssbd05.exceptions.AppBaseException;
 
 public class ExtraServiceAlreadyExistsException extends AppBaseException {
-    static final public String KEY_EXTRA_SERVICE_EXISTS= "error.extraservice.exists";
-
     public ExtraServiceAlreadyExistsException(String message) {
         super(message);
     }
