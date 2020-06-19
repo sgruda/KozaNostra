4768587f6463bf62d6c5bd2cd7666cfe1c73b0b8
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-06-19 23:20:45 +0200

    Usunięcie roli editReservationClient

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/web/mor/EditReservationController.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/web/mor/EditReservationController.java
@@ -99,7 +99,6 @@ public class EditReservationController implements Serializable {
 
     }
 
-    @RolesAllowed("editReservationClient")
     public void editReservation() {
 
         reservationDTO.setEventTypeName(eventTypeName);
