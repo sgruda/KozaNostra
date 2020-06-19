b93650f3e28bc3151f0873c776e02de588e99f92
Szymon Rutkowski <s.rutkowski235@gmail.com>
2020-06-19 20:37:54 +0200

    wyliczanie czasu trwania wydarzenia

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/utils/DateFormatter.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/utils/DateFormatter.java
@@ -1,17 +1,17 @@
 package pl.lodz.p.it.ssbd2020.ssbd05.utils;
 
+import lombok.extern.java.Log;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
-
+@Log
 public class DateFormatter {
 
     public static final String WITH_SECONDS= "yyyy-MM-dd HH:mm:ss";
     public static final String WITHOUT_SECONDS= "yyyy-MM-dd HH:mm";
-    static final int MINUTES_PER_HOUR = 60;
     static final int SECONDS_PER_MINUTE = 60;
-    static final int SECONDS_PER_HOUR = SECONDS_PER_MINUTE * MINUTES_PER_HOUR;
 
     public static String formatDate(LocalDateTime date) {
         if (date == null) {
@@ -30,21 +30,10 @@ public class DateFormatter {
     }
 
     public static int getHours(LocalDateTime start, LocalDateTime end) {
-
-        Period period = getPeriod(start,end);
-        int returnedhours = period.getDays()*24;
-        LocalDateTime today = LocalDateTime.of(end.getYear(),
-                end.getMonthValue(), end.getDayOfMonth(), start.getHour(), start.getMinute(), start.getSecond());
-        Duration duration = Duration.between(today, end);
-
-        long seconds = duration.getSeconds();
-
-        long hours = seconds / SECONDS_PER_HOUR;
-        long minutes = ((seconds % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE);
-        long secs = (seconds % SECONDS_PER_MINUTE);
-
-        returnedhours += hours;
-        if(minutes>0 || secs>0)
+        Duration duration =Duration.between(start,end);
+        long minutes = duration.toMinutes()%SECONDS_PER_MINUTE;
+        int returnedhours = (int) duration.toHours();
+        if(minutes>0)
             returnedhours+=1;
 
         return returnedhours;
