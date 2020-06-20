fd051aaf2881e32a8f61d1ba203f15854cd9529e
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-06-20 16:33:37 +0200

    Javadoc dla mapperów

--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/dto/mappers/mor/ClientMapper.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/dto/mappers/mor/ClientMapper.java
@@ -3,7 +3,18 @@ package pl.lodz.p.it.ssbd2020.ssbd05.dto.mappers.mor;
 import pl.lodz.p.it.ssbd2020.ssbd05.dto.mor.ClientDTO;
 import pl.lodz.p.it.ssbd2020.ssbd05.entities.mok.Client;
 
+/**
+ * Klasa konwertująca pomiędzy obiektami klas Client i ClientDTO.
+ * Jest ona wykorzystywana przez bibliotekę MapStruct w implementacji interfejsu ReservationMapper.
+ */
 public class ClientMapper {
+
+    /**
+     * Metoda odpowiedzialna za konwersję z obiektu klasy Client na ClientDTO.
+     *
+     * @param client Obiekt klasy Client.
+     * @return Obiekt klasy ClientDTO.
+     */
     ClientDTO toClientDTO(Client client) {
         return new ClientDTO(client.getAccount().getLogin(),
                 client.getAccount().getFirstname(),
--- src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/dto/mappers/mos/AddressMapper.java
+++ src/main/java/pl/lodz/p/it/ssbd2020/ssbd05/dto/mappers/mos/AddressMapper.java
@@ -8,13 +8,39 @@ import pl.lodz.p.it.ssbd2020.ssbd05.entities.mos.Address;
 
 import java.util.Collection;
 
+/**
+ * Interfejs konwertujący pomiędzy obiektami klas Address i AddressDTO,
+ * którego implementacja jest generowana poprzez bibliotekę MapStruct w trakcie kompilacji.
+ */
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AddressMapper {
 
+    /**
+     * Instancja implementacji interfejsu pobierana poprzez klasę narzędziową Mappers.
+     */
     AddressMapper INSTANCE = Mappers.getMapper(AddressMapper.class);
 
+    /**
+     * Metoda odpowiedzialna za konwersję z obiektu klasy Address na AddressDTO.
+     *
+     * @param address Obiekt klasy Address.
+     * @return Obiekt klasy AddressDTO.
+     */
     AddressDTO toAddressDTO(Address address);
+
+    /**
+     * Metoda odpowiedzialna za konwersję z obiektu klasy AddressDTO na Address.
+     *
+     * @param addressDTO Obiekt klasy AddressDTO.
+     * @return Obiekt klasy Address.
+     */
     Address createNewAddress(AddressDTO addressDTO);
 
+    /**
+     * Metoda odpowiedzialna za konwersję kolekcji obiektów klasy Address na kolekcję obiektów AddressDTO.
+     *
+     * @param addressCollection Kolekcja obiektów klasy Address.
+     * @return Kolekcja obiektów klasy AddressDTO.
+     */
     Collection<AddressDTO> toAddressDTOCollection(Collection<Address> addressCollection);
 }
